e2a1a74ad21decb531de07718fd0b3d94a5d7bed
Eric Biggers <ebiggers@google.com>
2019-01-14 15:57:16 -0800

    fs-verity: add setup code, UAPI, and Kconfig
    Add the beginnings of fs-verity support, including:
    
    - The fs-verity Kconfig option (CONFIG_FS_VERITY)
    - The fs-verity UAPI declarations (uapi/linux/fsverity.h)
    - The internal API header for filesystems to use (linux/fsverity.h)
    - The "setup" code which parses the fs-verity descriptor to create an
      fsverity_info structure that is attached to the in-memory inode; this
      structure describes the Merkle tree properties and contains the file
      measurement.  This is called from the ->open() and ->getattr() hooks.
    - Hash algorithm management; initially supporting SHA-256 only.
    
    The actual ->readpages() data verification, the ioctl implementations,
    ext4 and f2fs support, and other functionality comes in later patches.
    
    For more information about fs-verity, see the documentation file
    Documentation/filesystems/fsverity.rst.
    
    Signed-off-by: Eric Biggers <ebiggers@google.com>

---
+++ include/linux/fsverity.h
@@ -0,0 +1,63 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * fs-verity: read-only file-based authenticity protection
+ *
+ * Copyright 2018 Google LLC
+ */
+
+#ifndef _LINUX_FSVERITY_H
+#define _LINUX_FSVERITY_H
+
+#include <linux/fs.h>
+#include <uapi/linux/fsverity.h>
+
+/*
+ * fs-verity operations for filesystems
+ */
+struct fsverity_operations {
+	int (*set_verity)(struct inode *inode, loff_t data_i_size);
+	int (*get_metadata_end)(struct inode *inode, loff_t *metadata_end_ret);
+	struct page *(*read_metadata_page)(struct inode *inode, pgoff_t index);
+};
+
+#ifdef CONFIG_FS_VERITY
+
+/* setup.c */
+extern int fsverity_file_open(struct inode *inode, struct file *filp);
+extern int fsverity_prepare_setattr(struct dentry *dentry, struct iattr *attr);
+extern int fsverity_prepare_getattr(struct inode *inode);
+extern void fsverity_cleanup_inode(struct inode *inode);
+extern loff_t fsverity_full_i_size(const struct inode *inode);
+
+#else /* !CONFIG_FS_VERITY */
+
+/* setup.c */
+
+static inline int fsverity_file_open(struct inode *inode, struct file *filp)
+{
+	return IS_VERITY(inode) ? -EOPNOTSUPP : 0;
+}
+
+static inline int fsverity_prepare_setattr(struct dentry *dentry,
+					   struct iattr *attr)
+{
+	return IS_VERITY(d_inode(dentry)) ? -EOPNOTSUPP : 0;
+}
+
+static inline int fsverity_prepare_getattr(struct inode *inode)
+{
+	return -EOPNOTSUPP;
+}
+
+static inline void fsverity_cleanup_inode(struct inode *inode)
+{
+}
+
+static inline loff_t fsverity_full_i_size(const struct inode *inode)
+{
+	return i_size_read(inode);
+}
+
+#endif	/* !CONFIG_FS_VERITY */
+
+#endif	/* _LINUX_FSVERITY_H */
