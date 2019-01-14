1d9fa25a3ded86a19bdc5d324dc2b0a0a1922f7e
Eric Biggers <ebiggers@google.com>
2019-01-14 15:57:17 -0800

    fs-verity: implement FS_IOC_ENABLE_VERITY ioctl
    Add a function for filesystems to call to implement the
    FS_IOC_ENABLE_VERITY ioctl.  This ioctl enables fs-verity on a file,
    after userspace has appended verity metadata to it.
    
    This ioctl is documented in Documentation/filesystems/fsverity.rst;
    see there for more information.
    
    Signed-off-by: Eric Biggers <ebiggers@google.com>

--- include/linux/fsverity.h
+++ include/linux/fsverity.h
@@ -22,6 +22,9 @@ struct fsverity_operations {
 
 #ifdef CONFIG_FS_VERITY
 
+/* ioctl.c */
+extern int fsverity_ioctl_enable(struct file *filp, const void __user *arg);
+
 /* setup.c */
 extern int fsverity_file_open(struct inode *inode, struct file *filp);
 extern int fsverity_prepare_setattr(struct dentry *dentry, struct iattr *attr);
@@ -40,6 +43,14 @@ static inline bool fsverity_check_hole(struct inode *inode, struct page *page)
 }
 
 #else /* !CONFIG_FS_VERITY */
+
+/* ioctl.c */
+
+static inline int fsverity_ioctl_enable(struct file *filp,
+					const void __user *arg)
+{
+	return -EOPNOTSUPP;
+}
 
 /* setup.c */
 
